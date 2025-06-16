5e908ba66a320699791bfb6e5131c615685c33c3
vanhieuhp <vanhieuit10@gmail.com>
2025-06-16 22:47:07 +0700

    init: Start project
    Signed-off-by: vanhieuhp <vanhieuit10@gmail.com>

---
+++ src/lambda/BasicLambdas.java
@@ -0,0 +1,25 @@
+package lambda;
+
+interface I {
+    void m();
+}
+
+public class BasicLambdas {
+    public static void main(String[] args) {
+        I i = new I() {
+            @Override
+            public void m() {
+                System.out.println("Hello World!");
+            }
+        };
+        i.m();
+
+        // Java 8 - Lambda expression
+        I lambdaI = () -> {
+            System.out.println("Lambda version");
+        };
+        I lambdaI2 = () -> System.out.println("Lambda version 2");
+        lambdaI.m();
+        lambdaI2.m();
+    }
+}
