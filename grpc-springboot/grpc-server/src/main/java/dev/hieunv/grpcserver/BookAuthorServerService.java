e95acf5f620c26a50f8fe4f8c97724a2463eda17
vanhieuhp <vanhieuit10@gmail.com>
2025-09-25 10:08:22 +0700

    feat(grpc-server): add log
    Signed-off-by: hieunv <hieunv@epayjsc.com>

--- grpc-springboot/grpc-server/src/main/java/dev/hieunv/grpcserver/BookAuthorServerService.java
+++ grpc-springboot/grpc-server/src/main/java/dev/hieunv/grpcserver/BookAuthorServerService.java
@@ -4,16 +4,19 @@ import dev.hieunv.grpc.Author;
 import dev.hieunv.grpc.BookAuthorServiceGrpc;
 import dev.hieunv.grpc.TempDb;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.grpc.server.service.GrpcService;
 
 import java.util.Optional;
 
+@Slf4j
 @GrpcService
 public class BookAuthorServerService extends BookAuthorServiceGrpc.BookAuthorServiceImplBase {
 
     @Override
     public void getAuthor(Author request, StreamObserver<Author> responseObserver) {
         try {
+            log.info("getAuthor: {}", request);
             Optional<Author> author = TempDb.getAuthorsFromTempDb().stream()
                     .filter(a -> a.getAuthorId() == request.getAuthorId())
                     .findFirst();
