be0f0e80a2ef9f089b7baf40e2f36bd85e3ad729
hieunv <hieunv@epayjsc.com>
2026-04-21 15:21:20 +0700

    app(bankos): [kafka] convert to spring 3.5

--- exercises/bankos/src/main/java/dev/hieunv/bankos/config/KafkaConfig.java
+++ exercises/bankos/src/main/java/dev/hieunv/bankos/config/KafkaConfig.java
@@ -2,12 +2,6 @@ package dev.hieunv.bankos.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.ConsumerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
-import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
 
 @Configuration
